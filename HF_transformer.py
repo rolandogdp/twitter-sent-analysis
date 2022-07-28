968b315d7b76429e736ec09d82ebf3dda9240403
ktroyan <klim.troyan@gmail.com>
2022-07-28 04:04:03 +0200

    ALMOST solve subsets issue + small changes

--- HF_transformer.py
+++ HF_transformer.py
@@ -24,6 +24,7 @@ from datasets import load_dataset
 
 import time
 import requests
+import wandb
 
 
 # Config file
@@ -37,9 +38,10 @@ red = 15158332
 green = 3066993
 orange = 15105570
 
-# Global variables
-metric = load_metric("accuracy")    # metric to use
 
+# Global variables
+METRIC = load_metric("accuracy")    # metric to use
+val_dataset_global = None
 
 # DATASET CLASSES
 class TrainDataset(Dataset):
@@ -76,34 +78,55 @@ def read_file(file_name_label_tuple, starting_line=0, end_line=0):
 
     return(tweets, labels)
 
+# Read a file containing tweets and store them as well as their respective labels 
+def read_file_HF(file_name, starting_line=0, end_line=0):
+    tweets, labels = [], []
+    with open( file_name, 'r', encoding='utf-8') as f:
+        tweets = [line.split("\t",maxsplit=1) for line in f.readlines()[starting_line:end_line]]
+        tweets = np.array(tweets)
+        labels = tweets[:,0].astype(np.uint8)
+        tweets = tweets[:,1]
+        
+    return(tweets, labels)
+
 # Store the training data, tweets and labels, in numpy arrays
 def load_train_data(amount_per_batch, iteration):
  
-    if use_full_dataset == True:
-        X_train_neg_path = project_path + "train_neg_full.txt"
-        X_train_pos_path = project_path + "train_pos_full.txt"
+    # if use_full_dataset == True:
+    #     X_train_neg_path = project_path + "train_neg_full.txt"
+    #     X_train_pos_path = project_path + "train_pos_full.txt"
         
-    else:
-        X_train_neg_path = project_path + "train_neg.txt"
-        X_train_pos_path = project_path + "train_pos.txt"
+    # else:
+    #     X_train_neg_path = project_path + "train_neg.txt"
+    #     X_train_pos_path = project_path + "train_pos.txt"
 
-    amount_per_batch = amount_per_batch // 2
+    # amount_per_batch = amount_per_batch // 2
     
+    # starting_line = iteration * amount_per_batch
+    # end_line = starting_line + amount_per_batch
+    # print(f"Going to read {amount_per_batch*2} lines ({amount_per_batch} in each of the pos and neg datasets), starting_line:{starting_line}, end_line:{end_line}")
+    # tweets, labels = read_file((X_train_neg_path, 0), starting_line=starting_line, end_line=end_line)
+    # tweets_2, labels_2 = read_file((X_train_pos_path, 1), starting_line=starting_line, end_line=end_line)
+    # tweets += tweets_2
+    # tweets_2 = []
+    # del(tweets_2)
+    # labels += labels_2
+    # labels_2 = []
+    # del(labels_2)
+    # print(f"Loaded {len(tweets)} tweets!")
+    # return np.array(tweets), np.array(labels)
+
     starting_line = iteration * amount_per_batch
     end_line = starting_line + amount_per_batch
     print(f"Going to read {amount_per_batch*2} lines ({amount_per_batch} in each of the pos and neg datasets), starting_line:{starting_line}, end_line:{end_line}")
-    tweets, labels = read_file((X_train_neg_path, 0), starting_line=starting_line, end_line=end_line)
-    tweets_2, labels_2 = read_file((X_train_pos_path, 1), starting_line=starting_line, end_line=end_line)
-    tweets += tweets_2
-    tweets_2 = []
-    del(tweets_2)
-    labels += labels_2
-    labels_2 = []
-    del(labels_2)
-        
+    tweets, labels = read_file_HF(project_path + "HF_data.txt", starting_line, end_line)
     print(f"Loaded {len(tweets)} tweets!")
+
+
+    return tweets, labels
+
+
     
-    return np.array(tweets), np.array(labels)
 
 # Store the testing data (tweets) in a numpy array
 def load_test_data():
@@ -127,34 +150,57 @@ def vectorize_data(tweets, train_indices, val_indices):
     return X_train, X_val
 
 def get_train_val_data(tweets, labels):
-    nb_of_samples = len(tweets)
-    shuffled_indices = np.random.permutation(nb_of_samples)
-    split_idx = int(train_val_ratio * nb_of_samples)
+    global val_dataset_global
+    if val_dataset_global is None:
+        nb_of_samples = len(tweets)
+        shuffled_indices = np.random.permutation(nb_of_samples)
+        split_idx = int(train_val_ratio * nb_of_samples)
 
-    train_indices = shuffled_indices[:split_idx]
-    val_indices = shuffled_indices[split_idx:]
+        train_indices = shuffled_indices[:split_idx]
+        val_indices = shuffled_indices[split_idx:]
 
-    print("Number of indices for training: ", len(train_indices))
-    print("Number of indices for validation: ", len(val_indices))
+        print("Number of indices for training: ", len(train_indices))
+        print("Number of indices for validation: ", len(val_indices))
 
-    if use_most_freq_words:
-        X_train, X_val = vectorize_data(tweets, train_indices, val_indices)
+        if use_most_freq_words:
+            X_train, X_val = vectorize_data(tweets, train_indices, val_indices)
+        else:
+            X_train, X_val = tweets[train_indices], tweets[val_indices]
+        
+        Y_train = labels[train_indices]
+        Y_val = labels[val_indices]
+        
+        X_train = tokenizer(X_train.tolist(), max_length=config.tokenizer_max_length, padding="max_length", truncation=True)
+        X_val = tokenizer(X_val.tolist(), max_length=config.tokenizer_max_length, padding="max_length", truncation=True)
+
+        Y_train = torch.tensor(Y_train).clone().detach()
+        Y_val = torch.tensor(Y_val).clone().detach()
+
+        train_dataset = TrainDataset(X_train, Y_train)
+        val_dataset = TrainDataset(X_val, Y_val)
+        val_dataset_global = val_dataset
+        
+        return train_dataset, val_dataset
     else:
-        X_train, X_val = tweets[train_indices], tweets[val_indices]
-    
-    Y_train = labels[train_indices]
-    Y_val = labels[val_indices]
-    
-    X_train = tokenizer(X_train.tolist(), max_length=config.tokenizer_max_length, padding="max_length", truncation=True)
-    X_val = tokenizer(X_val.tolist(), max_length=config.tokenizer_max_length, padding="max_length", truncation=True)
+        nb_of_samples = len(tweets)
+        shuffled_indices = np.random.permutation(nb_of_samples)
+        
+        train_indices = shuffled_indices
+        print("Number of indices for training: ", len(train_indices))
 
-    Y_train = torch.tensor(Y_train).clone().detach()
-    Y_val = torch.tensor(Y_val).clone().detach()
+        if use_most_freq_words:
+            X_train = vectorize_data(tweets, train_indices, val_indices)
+        else:
+            X_train = tweets[train_indices]
+        
+        Y_train = labels[train_indices]
+        
+        X_train = tokenizer(X_train.tolist(), max_length=config.tokenizer_max_length, padding="max_length", truncation=True)
 
-    train_dataset = TrainDataset(X_train, Y_train)
-    val_dataset = TrainDataset(X_val, Y_val)
-    
-    return train_dataset, val_dataset
+        Y_train = torch.tensor(Y_train).clone().detach()
+        train_dataset = TrainDataset(X_train, Y_train)
+        
+        return train_dataset,val_dataset_global
 
 def get_test_data(tweets):
     nb_of_samples = len(tweets)
@@ -168,10 +214,10 @@ def compute_metrics(eval_pred):
     logits, labels = eval_pred      # here, we have to get rid of the second element (neutral class) of the logits before taking the softmax IF we want to only predict neg/pos
     predictions = np.argmax(logits, axis=-1)
 
-    return metric.compute(predictions=predictions, references=labels)
+    return METRIC.compute(predictions=predictions, references=labels)
 
 
-def train(model, train_dataset, val_dataset):
+def train(model, train_dataset, val_dataset, iteration):
 
     training_args = TrainingArguments(output_dir=checkpoints_path, 
                                     overwrite_output_dir=True,
@@ -190,7 +236,9 @@ def train(model, train_dataset, val_dataset):
                                     logging_dir='./logs',                   # directory for storing logs
                                     logging_steps=500,
                                     load_best_model_at_end=True,
-                                    num_train_epochs=config.n_epochs)
+                                    num_train_epochs=config.n_epochs,
+                                    report_to="wandb" # WANDB INTEGRATION
+                                    )
 
 
     # data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True)
@@ -205,9 +253,13 @@ def train(model, train_dataset, val_dataset):
       compute_metrics=compute_metrics
     )
 
-    trainer.train()
+    if len(os.listdir(checkpoints_path)) == 0:
+        trainer.train()
+    else: 
+        trainer.train(resume_from_checkpoint=True)
 
-    trainer.save_model(experiments_results_path + "/best_model")    # save the best model
+    best_model_at_iteration_path = f"/best_model/iteration{iteration}"
+    trainer.save_model(experiments_results_path + best_model_at_iteration_path)    # save the best model of the current iteration
 
 
 def load_model_from_checkpoint(selected_checkpoint):
@@ -272,11 +324,11 @@ def load_and_train(model, amount_per_batch, iteration):
         del(labels)
     
     # TRAINING
-    train(model, train_dataset, val_dataset)
+    train(model, train_dataset, val_dataset, iteration)
 
     return model
 
-# DISCORD
+# DISCORD 
 def send_discord_notif(title, content, color, error=None):
     if not discord_enabled:
         return
@@ -318,7 +370,8 @@ def submit_preds_on_Kaggle(submit_filename, msg):
 def run_training(model):
     print(f"Going to iterate over {number_of_iterations} subsets of {amount_per_it} samples/tweets (separated for training/validation) to see {total_amount_of_tweets} in total.")
     send_discord_notif("Starting Training", f"Going to iterate over {number_of_iterations} subsets of {amount_per_it} samples/tweets (separated for training/validation) to see {total_amount_of_tweets} in total.", orange, None)
-
+    project_name = f"cil-{model_name}".replace("/","-").replace("\\","").replace("?","").replace("%","").replace(":","")
+    wandb.init(project=project_name)
     try:
         total_subsets = range(number_of_iterations)[config.start_at_it:]
 
@@ -397,6 +450,7 @@ if __name__ == "__main__":
     np.random.seed(SEED)
     torch.manual_seed(SEED)
     torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = True # DEBUG: comment this when debugging.
 
     # Save in the experiment folder the command line that was used to run this program
     cmd = sys.argv[0] + ' ' + ' '.join(sys.argv[1:])
@@ -470,4 +524,3 @@ if __name__ == "__main__":
     # Time that took the whole experiment to run
     time_run = time.time() - time_run
     print(f"The program took {str(time_run/60)[:6]} minutes to run.")
-
