correct CA-KL model

SCAL_transformer.py
@@ -46,6 +46,7 @@ lr = 0.0
 curr_iter = 0
 val_dataset_global = None
 
+#standard script for starting training and testing, parsing arguments. Preprocessing is added
 def preprocess(text):
     new_text = []
     for t in text.split(" "):
@@ -109,32 +110,6 @@ def read_file_HF(file_name, starting_line=0, end_line=0):
     return(tweets, labels)
 
 def load_train_data(amount_per_batch, iteration):
-    '''
-    if use_full_dataset == True:
-        X_train_neg_path = project_path + "train_neg_full.txt"
-        X_train_pos_path = project_path + "train_pos_full.txt"
-        
-    else:
-        X_train_neg_path = project_path + "train_neg.txt"
-        X_train_pos_path = project_path + "train_pos.txt"
-
-    amount_per_batch = amount_per_batch // 2
-    
-    starting_line = iteration * amount_per_batch
-    end_line = starting_line + amount_per_batch
-    print(f"Going to read {amount_per_batch*2} lines ({amount_per_batch} in each of the pos and neg datasets), starting_line:{starting_line}, end_line:{end_line}")
-    tweets, labels = read_file((X_train_neg_path, 0), starting_line=starting_line, end_line=end_line)
-    tweets_2, labels_2 = read_file((X_train_pos_path, 1), starting_line=starting_line, end_line=end_line)
-    tweets += tweets_2
-    tweets_2 = []
-    del(tweets_2)
-    labels += labels_2
-    labels_2 = []
-    del(labels_2)
-    print(f"Loaded {len(tweets)} tweets!")
-    
-    return np.array(tweets), np.array(labels)
-    '''
     starting_line = iteration * amount_per_batch
     end_line = starting_line + amount_per_batch
     print(f"Going to read {amount_per_batch*2} lines ({amount_per_batch} in each of the pos and neg datasets), starting_line:{starting_line}, end_line:{end_line}")
@@ -522,7 +497,7 @@ if __name__ == "__main__":
     # Create the model
     #if model is None:
     #    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=config.num_labels, local_files_only=False, ignore_mismatched_sizes=True)
-    if model is None:
+    if model is None: #load CA-KL Model isntead
         model = MyScalModel(model_name = model_name, num_labels=config.num_labels, epsilon=config.epsilon, alpha = config.alpha,
                         beta = config.beta)
     model.to(C.DEVICE)  # automatic if use the Trainer()

models/myScalModel.py
@@ -20,6 +20,7 @@ class MyScalConfig(PretrainedConfig):
 		super().__init__(**kwargs)
 
 
+#compute bidirectional kl divergence
 def compute_kl_loss(p, q, pad_mask=None):
     
 		p_loss = F.kl_div(F.log_softmax(p, dim=-1), F.softmax(q, dim=-1), reduction='none')
@@ -42,14 +43,17 @@ def weights_init(m):
         torch.nn.init.xavier_uniform_(m.weight)
         torch.nn.init.zeros_(m.bias)
 
+#fast gradient sign method
 def fgsm_attack(embeddings, epsilon, embeddings_grad):
 	perturbed_embeddings = embeddings + epsilon*F.normalize(torch.sign(embeddings_grad), p=2.0, dim=1, eps=1e-9);
 	return perturbed_embeddings
 
+#fast gradient method
 def fgm_attack(embeddings, epsilon, embeddings_grad):
 	perturbed_embeddings = embeddings + epsilon*F.normalize(embeddings_grad, p=2.0, dim=1, eps=1e-9);
 	return perturbed_embeddings
 
+#contrastive loss implemented using log softmax and cosine similarity
 def loss_contrastive(h_true, h_adversarial, temperature):
 	batch_size = h_true.shape[0]
 	loss = 0.0
@@ -65,6 +69,7 @@ def loss_contrastive(h_true, h_adversarial, temperature):
 	loss = loss/batch_size
 	return loss
 
+#contextPooler taken from deberta pooler
 class ContextPooler(nn.Module):
     def __init__(self, hidden_size, dropout_prob = 0.1):
         super().__init__()
@@ -86,6 +91,7 @@ class ContextPooler(nn.Module):
     def output_dim(self):
         return self.hidden_size
 
+#my CA-KL Model
 class MyScalModel(PreTrainedModel):
 	def __init__(self, config = None, model_name = "microsoft/deberta-v3-base", num_labels = 2, temperature = 0.05, epsilon = 0.2, alpha = 0.5, beta = 1.0):
 		model = AutoModel.from_pretrained(model_name, num_labels = num_labels)
@@ -105,7 +111,7 @@ class MyScalModel(PreTrainedModel):
 		self.pooler = ContextPooler(self.features_dim, 0.1)
 		self.output_dim = self.features_dim
 		self.classifier = nn.Linear(self.output_dim, self.num_labels)
-		#For contrastive learning
+		#For contrastive learning, use a MLP to map features.
 		self.mlp = nn.Sequential(
 			nn.Linear(self.features_dim, 1024),
 			nn.GELU(),
@@ -116,7 +122,8 @@ class MyScalModel(PreTrainedModel):
 		self.mlp.apply(weights_init)
 		self.pooler.apply(weights_init)
 		weights_init(self.classifier)
-		self.temperature = temperature #temperature for ct loss, taken from the paper
+		self.temperature = temperature #temperature for ct loss, smaller temparature => pointy distribution
+		#hyperparameters
 		self.epsilon = epsilon
 		self.alpha = alpha
 		self.beta = beta
@@ -135,17 +142,16 @@ class MyScalModel(PreTrainedModel):
         output_hidden_states: Optional[bool] = None,
         return_dict = True,
         ):
-		#First run of true embedding and predictions
+		
 		if attention_mask is None:
 			attention_mask = torch.ones_like(input_ids)
 		if token_type_ids is None:
 			token_type_ids = torch.zeros_like(input_ids)
-		
-		#with torch.enable_grad():
+
+		#First run of true embedding and predictions
 		embedding_output = self.embeddings(input_ids.to(torch.long), token_type_ids.to(torch.long), 
 			position_ids, mask = attention_mask, inputs_embeds = inputs_embeds);
 		embeddingTrue = embedding_output
-
 		encoder_outputs = self.encoder(
             embeddingTrue,
             attention_mask,
@@ -156,11 +162,13 @@ class MyScalModel(PreTrainedModel):
 		encodedTrue_layers = encoder_outputs[1]
 		sequenceTrue_output = encodedTrue_layers[-1]
 		logitsTrue = self.classifier(self.pooler(sequenceTrue_output))
+
 		loss = torch.tensor(0.)
 		loss_ct = torch.tensor(0.)
 		loss_adversarial = torch.tensor(0.)
 		loss_kl = torch.tensor(0.)
 
+		#if condition for evaluation and testing
 		if labels is not None:
 			loss_fce = nn.CrossEntropyLoss()
 			loss = loss_fce(logitsTrue.view(-1, self.num_labels), labels.view(-1))
@@ -171,13 +179,11 @@ class MyScalModel(PreTrainedModel):
 			self.zero_grad();
 			loss.backward(retain_graph = True);
 			grad2 = embeddingTrue.grad.data
-			#embeddingTrue.requires_grad = False
 			self.zero_grad();
 			grad2.requires_grad = False
-			embeddingAdversarial = fgm_attack(embeddingTrue, self.epsilon, grad2)
+			embeddingAdversarial = fgm_attack(embeddingTrue, self.epsilon, grad2) #the embedding after pertubation
 
 			#do adversarial pass
-			#embedding_output['embeddings'] = embeddingAdversarial
 			encoder_outputs_adversarial = self.encoder(
 				embeddingAdversarial,
 				attention_mask,
@@ -189,7 +195,6 @@ class MyScalModel(PreTrainedModel):
 			sequenceAdversarial_output = encodedAdversarial_layers[-1]
 			logitsAdversarial = self.classifier(self.pooler(sequenceAdversarial_output))
 			loss_adversarial = loss_fce(logitsAdversarial.view(-1, self.num_labels), labels.view(-1))
-			#embedding_output['embeddings'] = embeddingTrue
 
 			#contrastive loss
 			h_true = self.mlp(sequenceTrue_output)
@@ -212,6 +217,7 @@ class MyScalModel(PreTrainedModel):
 			attentions = encoder_outputs.attentions
 		)
 
+	#self defined method for faster evaluating and testing
 	def predict(
 		self, 
 		input_ids: Optional[torch.Tensor] = None,