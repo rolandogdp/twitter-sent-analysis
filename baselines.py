dd13f3a697eeef5b74f871f457367d649aa8dc67
rgra <rgra@>
2022-07-31 18:09:21 +0200

    Commented the code, cleaned a bit

--- baselines.py
+++ baselines.py
@@ -1,15 +1,7 @@
-# import numpy as np
-# import spacy
-# import torch
-# import cupy
-# utilities
 import re
-# from turtle import title
 import numpy as np
 import pandas as pd
-# plotting
-# import seaborn as sns
-# from wordcloud import WordCloud
+
 import matplotlib.pyplot as plt
 # nltk
 from nltk.stem import WordNetLemmatizer
@@ -119,16 +111,17 @@ class Word2VecVectorizer:
 
 
 def load_train_data(amount=-1):
+    '''Load the training data from a file in the HF format ( label \t text)
+    takes as input the amount of lines to read from the begining. Left empty to read all the lines.  '''
     
     print(f"Going to read all data")
-    tweets, labels = read_file_HF("./HF_data.txt", 0, amount) #TODO: CHange this value
+    tweets, labels = read_file_HF("./HF_data.txt", 0, amount)
     print(f"Loaded {len(tweets)} tweets!")
-
-
     return tweets, labels
 
 def load_train_data_per_batch(amount_per_batch, iteration):
- 
+    '''Used to load the training data in batches instead of all at the same times, for low memory systems.
+    Not used in this code since 32go of ram is enough to run everything without issues.'''
 
     starting_line = iteration * amount_per_batch
     end_line = starting_line + amount_per_batch
@@ -140,6 +133,9 @@ def load_train_data_per_batch(amount_per_batch, iteration):
     return tweets, labels
 
 def read_file_HF(file_name, starting_line=0, end_line=0):
+    '''Helper function to read a file with the HF format ( label \t text).
+    takes the filename, the starting line and the ending line as parameters.
+    Will return the requested lines in 2 numpy arrays teets, labels.'''
     tweets, labels = [], []
     with open( file_name, 'r', encoding='utf-8') as f:
         tweets = [line.split("\t",maxsplit=1) for line in f.readlines()[starting_line:end_line]]
@@ -150,6 +146,7 @@ def read_file_HF(file_name, starting_line=0, end_line=0):
     return(tweets, labels)
 
 def check_libs():
+    '''Helper function to see if cuda is correctly installed, Unused in this code.'''
     print(torch.version.cuda)
     print(torch.cuda.is_available() )
     # cupy.show_config()
@@ -157,6 +154,8 @@ def check_libs():
     return(torch.cuda.is_available())
 
 def load_tweets(full=False):
+    '''Old method of reading the files, in case the HF dataset.txt hasn't been generated.
+    This is deprecated, and load_train_data() should be used instead.'''
     if full:
        file_pos = "train_pos_full.txt"
        file_neg = "train_neg_full.txt"
@@ -175,6 +174,27 @@ def load_tweets(full=False):
     return labels,tweets
     
 def preprocess_data(labels,tweets,token_tool=0):
+    '''Preprocessing function to apply on the raw tweet's text. 
+    takes 3 params as entry: labels: np.array, tweets: np.array, token_tool: int
+    token_tool serves as index to which method of tokenization to use on the tweets.
+
+    returns:  X_train,X_test,y_train, y_test , shuffled and with test size being 0.005
+    Currently the best performing is the TFID_VECT from sklearn.
+
+    token_tool=0: uses the tokenizer from the bertweet model from hugging face. Took this one since
+    it was one of the best performing one. How ever it yields poor results on the baselines because 
+    it produces sparse results which don't perform well on simple models.
+
+    token_tool=1: Uses the Count vectorizer from sklearn, This has been disabled as it did not produce good results,
+    and took quite a while to run.
+
+    token_tool=2: Uses sklearn hashvectorizer, it yields pretty good results and is also quite fast.
+
+    token_tool=3: This is the one performing the best results, it uses sklearn TFID vectorizer. 
+
+    token_tool=4: This one is disabled as running with glove resulted in issues with hardware limations/space.
+
+    '''
     
     # lm = nltk.WordNetLemmatizer()
     # stop_words=set(stopwords.words("english"))
@@ -183,6 +203,7 @@ def preprocess_data(labels,tweets,token_tool=0):
     should_split = True
 
     if token_tool == 0 :
+        
         tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-base", use_fast=True)
         tweets_out = tokenizer(tweets.tolist(), max_length=128, padding="max_length")
         tweets_out = np.array(tweets_out["input_ids"])
@@ -223,9 +244,7 @@ def preprocess_data(labels,tweets,token_tool=0):
 
                 
     # print("Tweet processed:",tweets[0,:])
-    
 
-    # input()
     if should_split:
         X_train, X_test, y_train, y_test = train_test_split(tweets_out,labels,test_size = 0.005, random_state = 12345)
 
@@ -251,16 +270,19 @@ def dl_wordlists():
     nltk.download('stopwords')
 
 def gen_word2vec_file():
+    '''Generates the word2vec format from the glove huge dataset.
+    only required if working with token_tool=4 '''
     word2vec_output_file = "./glove.twitter.27B.200d.word2vec"
     glove2word2vec("./glove.twitter.27B.200d.txt", word2vec_output_file)
 
 def main():
-    # dl_wordlists()
-    # check_libs()
-    # gen_word2vec_file()
+
+    # dl_wordlists() # enable to download wordlists if needed
+    # check_libs() # 
+    # gen_word2vec_file() # only needed for the  TOKENIZER_GENSIM_WORD2VEC
     tweets, labels = load_train_data(500000)
     preprocessing_methods = ["TOKENIZER_HF_TOKENIZER_128" ,"TOKENIZER_SKLEARN_CountVectorizer" ,"TOKENIZER_SKLEARN_HASHVECTORIZER" ,"TOKENIZER_SKLEARN_TFID_VECT" ,"TOKENIZER_GENSIM_WORD2VEC" ]
-    should_skip_method = [False,True,False,False,True]
+    should_skip_method = [False,True,False,False,True] # Used to quickly enable/disable tokenization methods for testing which one produces best results.
     for i,method_name in enumerate(preprocessing_methods):
         if should_skip_method[i]:
             continue
@@ -275,7 +297,6 @@ def main():
         LRmodel = LogisticRegression(C = 2, max_iter = 1000, n_jobs=-1)
         NUSVCmodel = svm.NuSVC(gamma="auto")
         RandomForestClassifiermodel = RandomForestClassifier(n_estimators=200, max_depth=10)
-        # BiLSTM_SentimentAnalysismodel = BiLSTM_SentimentAnalysis()
 
         XGB_CLASSIFIER = xgb.XGBClassifier()
     
@@ -287,26 +308,32 @@ def main():
         model_fit(XGB_CLASSIFIER,X_train,X_test,y_train, y_test,name=f"XGB_CLASSIFIER-{method_name}")
         model_fit(RandomForestClassifiermodel,X_train,X_test,y_train, y_test,name=f"RandomForestClassifier-{method_name}")
         
-        # model_fit(NUSVCmodel,X_train,X_test,y_train, y_test,name="NonLinearSVC-{method_name}")
-        # model_fit(BiLSTM_SentimentAnalysismodel,X_train,X_test,y_train, y_test,name="BiLSTM_SentimentAnalysismodel-{method_name}")
+        # model_fit(NUSVCmodel,X_train,X_test,y_train, y_test,name="NonLinearSVC-{method_name}") # takes for ever to run..
     
 
 
 
 def model_fit(model,X_train,X_test,y_train, y_test, name="Default"):
+    '''Simple helper function to fit a given model (must follow sklearn api)
+    and evaluate the results.'''
     print("Starting to fit model: "+name)
     model.fit(X_train, y_train)
-    model_Evaluate(model,X_train,X_test,y_train, y_test,name=name)
-    y_pred1 = model.predict(X_test)
-    plot_roc_auc(y_test,y_pred1,name=name)
+
+    y_pred = model_Evaluate(model,X_test, y_test,name=name)
+    # y_pred1 = model.predict(X_test)
+    plot_roc_auc(y_test,y_pred,name=name)
     
 
-def model_Evaluate(model,X_train,X_test,y_train, y_test,name="Default"):
+def model_Evaluate(model,X_test, y_test,name="Default"):
+    '''Function taken online to provide simple plots, and prints 
+    the classification report from sklearn.
+    takes as input the model, the x_test, y_test, and a name for the model plots/prints/save path'''
     
-    # Predict values for Test dataset
+    # prediction and results
     y_pred = model.predict(X_test)
-    # Print the evaluation metrics for the dataset.
     print(classification_report(y_test, y_pred))
+
+    # Plots , using online code.
     # Compute and plot the Confusion matrix
     cf_matrix = confusion_matrix(y_test, y_pred)
     categories = ['Negative','Positive']
@@ -315,61 +342,15 @@ def model_Evaluate(model,X_train,X_test,y_train, y_test,name="Default"):
     labels = [f'{v1} n {v2}' for v1, v2 in zip(group_names,group_percentages)]
     print(labels)
     labels = np.asarray(labels).reshape(2,2)
+    plt.figure()
     sns.heatmap(cf_matrix, annot = labels, cmap = 'Blues',fmt = '',
     xticklabels = categories, yticklabels = categories)
     plt.xlabel("Predicted values", fontdict = {'size':14}, labelpad = 10)
     plt.ylabel("Actual values" , fontdict = {'size':14}, labelpad = 10)
     plt.title (f"Confusion Matrix model:{name}", fontdict = {'size':18}, pad = 20)
     plt.savefig(f"./images/Confusion_Matrix_model_{name}.png")
-    #plt.show()
+    return y_pred
 
-
-class BiLSTM_SentimentAnalysis(torch.nn.Module) :
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, dropout) :
-        super().__init__()
-
-        # The embedding layer takes the vocab size and the embeddings size as input
-        # The embeddings size is up to you to decide, but common sizes are between 50 and 100.
-        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
-
-        # The LSTM layer takes in the the embedding size and the hidden vector size.
-        # The hidden dimension is up to you to decide, but common values are 32, 64, 128
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
-
-        # We use dropout before the final layer to improve with regularization
-        self.dropout = nn.Dropout(dropout)
-
-        # The fully-connected layer takes in the hidden dim of the LSTM and
-        #  outputs a a 3x1 vector of the class scores.
-        self.fc = nn.Linear(hidden_dim, 3)
-
-    def forward(self, x, hidden):
-        """
-        The forward method takes in the input and the previous hidden state 
-        """
-
-        # The input is transformed to embeddings by passing it to the embedding layer
-        embs = self.embedding(x)
-
-        # The embedded inputs are fed to the LSTM alongside the previous hidden state
-        out, hidden = self.lstm(embs, hidden)
-
-        # Dropout is applied to the output and fed to the FC layer
-        out = self.dropout(out)
-        out = self.fc(out)
-
-        # We extract the scores for the final hidden state since it is the one that matters.
-        out = out[:, -1]
-        return out, hidden
-    
-    def init_hidden(self):
-        return (torch.zeros(1, batch_size, 32), torch.zeros(1, batch_size, 32))
-
-
-
-
-
-    
 
 
 if __name__ == "__main__":
